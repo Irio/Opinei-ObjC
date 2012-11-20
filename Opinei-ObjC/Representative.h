fa3593362e886808cfe6c4a9d321a07208174e66
Irio Irineu Musskopf Junior <irio.musskopf@caixadeideias.com.br>
2012-11-19 23:39:13 -0200

    Create interface with attributes of Representative

---
+++ Opinei-ObjC/Representative.h
@@ -0,0 +1,15 @@
+//
+//  Representative.h
+//  Opinei-ObjC
+//
+//  Created by Irio Irineu Musskopf Junior on 19/11/12.
+//  Copyright (c) 2012 Irio Irineu Musskopf Junior. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Representative : NSObject
+
+@property (readwrite) NSString *name;
+
+@end
---
+++ Opinei-ObjC/Representative.m
@@ -0,0 +1,13 @@
+//
+//  Representative.m
+//  Opinei-ObjC
+//
+//  Created by Irio Irineu Musskopf Junior on 19/11/12.
+//  Copyright (c) 2012 Irio Irineu Musskopf Junior. All rights reserved.
+//
+
+#import "Representative.h"
+
+@implementation Representative
+
+@end
