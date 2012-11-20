02c4cb82e6237f2c8bec984bd38b99d08abf88ca
Irio Irineu Musskopf Junior <irio.musskopf@caixadeideias.com.br>
2012-11-19 23:31:56 -0200

    Initial Commit

---
+++ Opinei-ObjC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Opinei-ObjC
+//
+//  Created by Irio Irineu Musskopf Junior on 19/11/12.
+//  Copyright (c) 2012 Irio Irineu Musskopf Junior. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
